d1cbc57f8920b983c58354b32c613800935bdb5a
LoicTramis <loic.tramis@gmail.com>
2024-07-08 23:41:43 +0200

    Add: Top 3 habit on HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,47 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import service from '../service/api'
+import React, { useEffect, useState } from "react";
+import service from "../service/api";
 
 // Display habits and groups
 // Display nothing if no habits and no groups
 type Habit = {
-  id: string,
-  title: string,
-  description: string,
-  startGoal: Date,
-  endGoal: Date,
-  frequency: string,
+  id: string;
+  title: string;
+  description: string;
+  startGoal: Date;
+  endGoal: Date;
+  frequency: string;
   creator: {
-    username: string,
-    email: string
-  },
-  difficulty: string,
-  groups: string[]
-}
-
+    username: string;
+    email: string;
+  };
+  difficulty: string;
+  groups: string[];
+};
 
 const HomePage = () => {
-  const [habits, setHabits] = useState<null | Habit[]>(null)
+  const [habits, setHabits] = useState<null | Habit[]>(null);
 
   const fetchHabits = async () => {
     try {
-      const response = await service.get("/api/habits")
-      setHabits(response.data)
+      const response = await service.get("/api/habits");
+      console.log(response.data);
+      setHabits(response.data);
     } catch (error) {
       // show some error on the screen
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchHabits()
-  }, [])
+    fetchHabits();
+  }, []);
 
   if (!habits) {
-    return <p>Loading...</p>
+    return <p>Loading...</p>;
   }
 
-  const habitsJSX = habits.map((habit: Habit, index) => {
-    if (index >= 3) return null
+  const filteredHabits = habits
+    .toSorted((habit1: Habit, habit2: Habit) => habit2.groups.length - habit1.groups.length)
+    .slice(0, 3);
+
+  const habitsJSX = filteredHabits.map((habit: Habit) => {
     return (
       <li key={habit.id}>
         <h3>{habit.title}</h3>
@@ -51,20 +54,17 @@ const HomePage = () => {
         {/* <p>Start: {Date(habit.startGoal).toString()}</p>
         <p>End: {habit.endGoal}</p> */}
         <p>Frequency: {habit.frequency}</p>
-        <p>Groups: {habit.groups}</p>
+        <p>Groups: {habit.groups.length}</p>
         <hr />
       </li>
-    )
-  })
-  // TODO DO THE SEARCH
+    );
+  });
 
   return (
-    <main className='w-5/6'>
-      <ul>
-        {habitsJSX}
-      </ul>
+    <main className="w-5/6">
+      <ul>{habitsJSX}</ul>
     </main>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
